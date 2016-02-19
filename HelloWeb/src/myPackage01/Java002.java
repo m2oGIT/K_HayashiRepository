efc6cda122785df5919bb0a802284f3834a8947e
Hayashi Kaori <hayashi.kaori@eiwa.com>
2016-02-20 07:26:46 +0900

    プロジェクトを追加

---
+++ HelloWeb/src/myPackage01/Java002.java
@@ -0,0 +1,24 @@
+/**
+ * Copyright 2015 EIS Co., Ltd. All rights reserved.
+ */
+package myPackage01;
+
+/**
+ *  コンソール表示 <br />
+ *  コンソールに表示する <br />
+ *  更新履歴 2015/07/05 （林花織）：新規作成 <br />
+ */
+public class Java002 {
+
+  /**
+   * メインメソッド 処理を起動します。 <br />
+   * コンソールに表示する <br />
+   * @param args 起動引数
+   */
+  public static void main(String[] args) {
+    // TODO 自動生成されたメソッド・スタブ
+    System.out.println(args[0]);
+
+  }
+
+}
